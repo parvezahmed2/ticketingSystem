b585e761dea37e3b2c4a40b49ee363138dd4e820
parvezahmed_24 <parvezahmed9432@gmail.com>
2026-07-10 17:07:38 +0600

    create booking tables

--- query.sql
+++ query.sql
@@ -26,3 +26,24 @@ CREATE TABLE matches (
     match_status VARCHAR(20) NOT NULL
         CHECK (match_status IN ('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
 );
+
+
+
+CREATE TABLE bookings (
+    booking_id INT PRIMARY KEY,
+    user_id INT NOT NULL,
+    match_id INT NOT NULL,
+    seat_number VARCHAR(20),
+    payment_status VARCHAR(20)
+        CHECK (payment_status IN ('Pending', 'Confirmed', 'Cancelled', 'Refunded')),
+    total_cost DECIMAL(10,2) NOT NULL
+        CHECK (total_cost >= 0),
+
+    CONSTRAINT fk_user
+        FOREIGN KEY (user_id)
+        REFERENCES users(user_id),
+
+    CONSTRAINT fk_match
+        FOREIGN KEY (match_id)
+        REFERENCES matches(match_id)
+);
