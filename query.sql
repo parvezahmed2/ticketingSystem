f4407a82015f1573b445280e9f10538d4b62ce1a
parvezahmed_24 <parvezahmed9432@gmail.com>
2026-07-10 17:02:21 +0600

    create matches table

--- query.sql
+++ query.sql
@@ -16,10 +16,13 @@ CREATE TABLE users (
 );
 
 
-
-SELECT *
-FROM information_schema.columns
-WHERE table_name = 'users';
-
-
-\d users
+ 
+CREATE TABLE matches (
+    match_id INT PRIMARY KEY,
+    fixture VARCHAR(100) NOT NULL,
+    tournament_category VARCHAR(100) NOT NULL,
+    base_ticket_price DECIMAL(10,2) NOT NULL
+        CHECK (base_ticket_price >= 0),
+    match_status VARCHAR(20) NOT NULL
+        CHECK (match_status IN ('Available', 'Selling Fast', 'Sold Out', 'Postponed'))
+);
