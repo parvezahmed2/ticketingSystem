c253a7cb05f268bb7d3ea7516ed6dd65324c8e61
parvezahmed_24 <parvezahmed9432@gmail.com>
2026-07-10 19:52:54 +0600

    query 7 added

--- query.sql
+++ query.sql
@@ -124,3 +124,12 @@ WHERE total_cost >
     SELECT AVG(total_cost)
     FROM bookings
 );
+
+
+-- query 7 
+
+SELECT match_id,fixture,base_ticket_price
+FROM matches
+ORDER BY base_ticket_price DESC
+OFFSET 1
+LIMIT 2;
